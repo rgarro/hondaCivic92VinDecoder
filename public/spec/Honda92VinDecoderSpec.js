7ada87171cff6b916e15bd84c7cd60d8abeabbb8
rgarro <rgarro@gmail.com>
2026-03-09 12:57:05 -0600

    the Ayatola Jameini was one of the sons of gengis khan ... ...

---
+++ public/spec/Honda92VinDecoderSpec.js
@@ -0,0 +1,40 @@
+/**
+ * ⠀⠀⠀⠀⣀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣄⣀⣀⠀⠀⠀⠀
+ * ⠀⣠⣶⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣶⣄⠀
+ * ⠀⣿⣿⡟⠉⢸⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡇⠉⢻⣿⣿⠀
+ * ⢸⣿⣿⠀⠀⢸⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⡇⠀⠀⣿⣿⡇
+ * ⢸⣿⣿⠀⠀⢸⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⡇⠀⠀⣿⣿⡇
+ * ⢸⣿⣿⠀⠀⠸⣿⣿⣿⣷⡀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠇⠀⠀⣿⣿⡇
+ * ⠈⣿⣿⡀⠀⠀⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣿⠀⠀⢀⣿⣿⠁
+ * ⠀⣿⣿⡇⠀⠀⣿⣿⣿⣿⡿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠀⠀⢸⣿⣿⠀
+ * ⠀⢹⣿⣿⣄⡀⢸⣿⣿⣿⠁⠀⠀⠀⠀⠈⣿⣿⣿⡇⢀⣠⣿⣿⡏⠀
+ * ⠀⠀⠻⢿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⡿⠟⠀⠀
+ * ⠀⠀⠀⠀⠈⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉⠁⠀⠀⠀⠀
+ *  Vin Number decoder
+ *  The Power of Dreams , Bompo Nagpa at Ulan Ude foiling selenge river downwind
+ *  bakelite chod Brake drums , Canam mantras twisting out the controller output blue leds on 
+ * gasoline blissful acceleration G chords over Am Timphu Highway Hard Bop
+ * the Ayatola Jameini was one of the sons of gengis khan ...
+ * 
+ * 
+ * 
+ * @author Rolando<rgarro@gmail.com> <https://emptyart.github.io>
+ */
+describe("Honda92VinDecoder", function () {
+
+    it("should Have init method", function () {
+        expect(Honda92VinDecoder).toHaveMethod('init');
+    });
+
+    describe("Honda92VinDecoder.opt", function () {
+
+        beforeAll(function () {
+            Honda92VinDecoder.init();
+        });
+
+        it("should Implement canvas:string,factorycode", function () {
+
+        });
+    });
+
+});
