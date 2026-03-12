771a8e49a808b4ec1c0f87f4c7cb1fa22c6d32c9
rgarro <rgarro@gmail.com>
2026-03-12 09:53:51 -0600

    D15B7 SOHC Spoon version Hawthorne dishwasher

--- public/js/Honda92VinDecoder.js
+++ public/js/Honda92VinDecoder.js
@@ -15,6 +15,11 @@
  *@author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
  */
 var Honda92VinDecoder = {
+
+    manufacturer: "2HG",//Honda of Canada MFG, kill kenny 
+    lineEngineBodyTypeCivic1500: "EH2",//civic 1500 2 door
+    lineEngineBodyTypeCivic1600: "EH3",//civic 1600 2 door
+
     init: function () {
 
     }
